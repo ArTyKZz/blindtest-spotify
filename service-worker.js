4b836c5b4b0b058f066e6050a2c70bd4e2af7d1c
Mickaël Ousset <mickael.ousset@grenoble-inp.org>
2021-04-15 16:39:50 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blindtest-spotify/precache-manifest.af1d10f296fb8bac425bec0e56195ae1.js"
+  "/blindtest-spotify/precache-manifest.e5ed35beebd320c378b9383c8d2129c7.js"
 );
 
 self.addEventListener('message', (event) => {
